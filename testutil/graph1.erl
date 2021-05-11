e8449f06c59b4b221361729da860f0c740c7daef
Zachary Kieda <kieda@users.noreply.github.com>
2021-05-10 19:25:40 -0600

    testing utilities

--- testutil/graph1.erl
+++ testutil/graph1.erl
@@ -2,7 +2,7 @@
 %%% @author zkieda
 %%% @copyright (C) 2021, <COMPANY>
 %%% @doc
-%%%
+%%%   basic test graph
 %%% @end
 %%% Created : 12. Apr 2021 10:26 PM
 %%%-------------------------------------------------------------------
@@ -10,4 +10,25 @@
 -author("zkieda").
 
 %% API
--export([]).
+-export([vertices/0, edges/0]).
+
+vertices() -> [
+  vertex_a,
+  vertex_b
+%  vertex_c,
+%  vertex_d,
+%  vertex_e,
+%  vertex_f,
+%  vertex_g
+].
+
+edges() -> [
+  {vertex_a, vertex_b}
+%  {vertex_a, vertex_d},
+%  {vertex_b, vertex_d},
+%  {vertex_b, vertex_c},
+%  {vertex_c, vertex_a},
+%  {vertex_f, vertex_c},
+%  {vertex_g, vertex_d},
+%  {vertex_d, vertex_a}
+].
--- testutil/worker_random_messenger.erl
+++ testutil/worker_random_messenger.erl
@@ -12,24 +12,23 @@
 
 %% API
 -export([new_state/1, update_state/2]).
--define(DEFAULT_MESSAGE_PROBABILITY, .25). % 25% chance of sending a message
--define(DEFAULT_WAIT_TIME, 25). % 25 millis after sending a message
+-define(DEFAULT_MESSAGE_PROBABILITY, 0.25). % 25% chance of sending a message
+-define(DEFAULT_WAIT_TIME, 2500). % 25 millis after sending a message
 
 new_state(VertexArgs) ->
   Probability = maps:get(worker_random_message_probability, VertexArgs, ?DEFAULT_MESSAGE_PROBABILITY),
   WaitTime = maps:get(worker_random_message_wait_time, VertexArgs, ?DEFAULT_WAIT_TIME),
-  #{unique_id := 0, message_probability := Probability, wait_time := WaitTime}.
+  #{unique_id => 0, message_probability => Probability, wait_time => WaitTime}.
 
 next_id(#{unique_id := I}) -> I.
 inc_id(State = #{unique_id := I}) ->
   maps:update(unique_id, I + 1, State).
 
 update_state(State, UserState = #{message_probability := P, wait_time := T}) ->
-  Outgoing = graph_state:get_outgoing(State),
-  SendMessagesTo = lists:filter(fun(_) -> random:uniform() =< P end, graph_state:get_outgoing_vertices(State)),
+  SendMessagesTo = lists:filter(fun(_) -> random:uniform() =< P end, graph_state:get_outgoing(State)),
   lists:foldl(fun(V, {StateA, UserStateA}) ->
-                Message = graph_state:get_vertex(State) ++ next_id(UserStateA),
-                StateB = messenger:send_message(StateA, maps:get(Outgoing, V), Message),
+                Message = lists:concat([graph_state:get_vertex(StateA), next_id(UserStateA)]),
+                StateB = messenger:send_message(Message, V, StateA),
                 UserStateB = inc_id(UserStateA),
                 timer:sleep(T),
                 {StateB, UserStateB}
